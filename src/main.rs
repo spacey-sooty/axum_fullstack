33f1828be1990491db6d70d2ed0499605acad3ff
Isaac Turner <spacey_sooty@outlook.com>
2023-11-19 22:22:02 +0800

    json no worky

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
-use axum::{routing::get, Router, extract::Json};
+use axum::{routing::get, Router};
 
 #[derive(serde::Deserialize, useful_macros::From)]
 struct Foo {
     message: String,
 }
 
-fn json_handler() -> dyn Fn(Json<Foo>) {
-    || async { axum::Json(Foo::from("Hello, World!".to_string())) }
+fn json_handler() -> axum::Json<Foo> {
+    axum::Json(Foo::from("Hello, World!".to_string()))
 }
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
     let app = Router::new()
-        .route("/", get(Box<json_handler>));
+        .route("/", get(json_handler));
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"127.0.0.1:8000".parse().unwrap())
